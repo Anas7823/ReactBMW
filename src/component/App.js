fe24a881d8ac245363fba8338ed779e136b36ca9
Anas7823 <anaselkhiat78@gmail.com>
2022-09-23 15:49:03 +0200

    nav/footer

--- src/component/App.js
+++ src/component/App.js
@@ -1,23 +1,16 @@
 import '../style/App.css';
+import Banner from './Banner';
+import Footer from './Footer';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+    <div>
+      <Banner />
+      
+      <Footer />
     </div>
-  );
+  )
+
 }
 
 export default App;
---
+++ src/component/banner.js
@@ -0,0 +1,9 @@
+import '../style/Banner.css';
+
+function Banner(){
+	return<div className="banner">
+        <h1>ReactBMW MotorSport</h1>
+    </div> 
+}
+
+export default Banner;
