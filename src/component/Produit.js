5cb41be3ab150435359dd5a1936012ad87c64160
Anas7823 <anaselkhiat78@gmail.com>
2022-09-23 16:22:18 +0200

    ajout photo + création page ^produit

---
+++ src/component/Produit.js
@@ -0,0 +1,18 @@
+import '../style/Produit.css';
+
+function Produit() {
+    const Produit = 
+    [{name: 'C1 Miteuse',
+    commentaire: "Avec ses 270cv et ses 250kg ce véhicule totalement homologé va vous satisfaire et vous donnera des sensations tel que celle d'un avion de chasse.",
+    image:,},
+
+    {name: "C5 de l'abberance",
+    commentaire: "Plus lourde que la C1, la C5 Pack M sport + vous garantit une vitesse de pointe et accéleration digne de son nom, cependant à voir si le freinage assume le poids et la puissance.",
+    image:},
+]
+}
+return(
+
+)
+
+export default Produit;
