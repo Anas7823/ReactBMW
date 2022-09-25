56801787ca12ba15d20ad1c0b9d05c1d26ee0013
Anas El Khiat <anas.elkhiat@agecco.io>
2022-09-25 16:44:25 +0200

    Route avec le Link (début de la page d'accueil)

--- src/component/Accueil.js
+++ src/component/Accueil.js
@@ -1,9 +1,22 @@
 import '../style/Accueil.css';
 
 function Accueil(){
-    return <div>
-        
-    </div>
+    return <div className='contenue'>
+        <p><h1>Bien venue sur le site de ReactBMW MotorSport</h1></p>        
+            <p><h3>Notre entreprise propose des modifications de véhicule et des prototypes 
+                demander par des clients ou communauté comme des projet entre deux marques 
+                automobiles différentes.</h3>
+            </p>
+            <div className='services'>
+                <div className='service'>
+                    <h3>Service 1</h3>
+                </div>
+                <div className='service'>
+                <h3>Service 2</h3>
+                </div>     
+                
+            </div> 
+        </div>
 }
 
 export default Accueil;
--- src/component/banner.js
+++ src/component/banner.js
@@ -1,9 +1,19 @@
 import '../style/Banner.css';
 import Logo from "../assets/BMW-Logo.png";
+import * as React from "react";
+import { Link } from "react-router-dom";
 
 function Banner(){
 	return<div className="banner">
-        <img src={Logo} alt="" className='logo'/><h1>ReactBMW MotorSport</h1>
+        <Link to='/'>
+        <img src={Logo} alt="" className='logo'/>
+        </Link>
+        <h1>ReactBMW MotorSport</h1>
+        <Link to='/Produit'>
+            <h4 className='ProduitNav'>Produit</h4>
+        </Link>
+            
+        {/* <a href='/produit'> <h4 className='ProduitNav'>Produit</h4> </a>  chemin avec la balise a*/}
     </div> 
 }
 
