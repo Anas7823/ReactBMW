f0d2674773e36b584ebf33bca0b97ffcf776ee61
Anas El Khiat <anas.elkhiat@agecco.io>
2022-10-01 16:44:31 +0200

    text accueil

--- src/component/Accueil.js
+++ src/component/Accueil.js
@@ -3,9 +3,9 @@ import '../style/Accueil.css';
 function Accueil(){
     return <div className='contenue'>
         <p><h1>Bien venue sur le site de ReactBMW MotorSport</h1></p>        
-            <p><h3>Notre entreprise propose des modifications de véhicule et des prototypes 
-                demander par des clients ou communauté comme des projet entre deux marques 
-                automobiles différentes.</h3>
+            <p>
+                <h3>Notre entreprise propose des modifications de véhicule et des prototypes 
+                demander par des clients ou communautés comme des préparations de véhicule atypique.</h3>
             </p>
             <div className='services'>
                 <div className='service'>
