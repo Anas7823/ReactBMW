1c570a8e15527cfbb4e0c8699c5cce5db2fa7a0e
Anas7823 <anaselkhiat78@gmail.com>
2022-10-03 15:30:33 +0200

    img

--- src/component/Accueil.js
+++ src/component/Accueil.js
@@ -2,23 +2,25 @@ import '../style/Accueil.css';
 
 function Accueil(){
     return <div className='contenue'>
-        <p><h1>Bien venue sur le site de ReactBMW MotorSport</h1></p>        
+        
+        <p><h1>Bienvenue sur le site de ReactBMW MotorSport</h1></p>        
             <p>
                 <h3>Notre entreprise propose des modifications de véhicule et des prototypes 
                 demander par des clients ou communautés comme des préparations de véhicule atypique.</h3>
             </p>
+
             <div className='services'>
                 <div className='service'>
-                    <h3>Service 1</h3>
-                    Une liste de service sera disponible prochainement
-
+                    <h3>Customisation de véhicule</h3>
+                    <img class="img-service" src="https://atelier-gesret.fr/wp-content/uploads/2018/11/LOGO-atelier-gesret-garagiste-a-domicile-blanc-2.png" alt=""></img>
                 </div>
+            
                 <div className='service'>
-                    <h3>Service 2</h3>
-                    Une liste de service sera disponible prochainement
+                    <h3>Création de prototypes</h3>
+                    <img class="img-service" src="https://upload.wikimedia.org/wikipedia/fr/5/5f/Cars_%28film%29_Logo.png" alt=""></img>
                 </div>     
-                
             </div> 
+        
         </div>
 }
 
