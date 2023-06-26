c943b75464a843e9ad31787572c8f9551dcb796b
ANAS EL KHIAT <anas@adame-techs.com>
2023-06-26 16:48:36 +0200

    lien accueil

--- src/component/Banner.js
+++ src/component/Banner.js
@@ -8,7 +8,7 @@ import { Link } from "react-router-dom";
 function Banner(props){
   let theme= props.mode
 	return<div className={` banner ${theme}`}>
-        <Link to='/Accueil'>
+        <Link to='/'>
             <img src={Logo} alt="" className='logo'/>
         </Link>
         <h1>ReactBMW MotorSport</h1>
